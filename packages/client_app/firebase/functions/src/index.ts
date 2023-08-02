d0a542e8874f947607fb6f06b2faaefb9cc7fe40
KoheiKanagu <kanagu@kingu.dev>
2023-08-02 09:42:22 +0900

    beforeUserCreated

--- packages/client_app/firebase/functions/src/index.ts
+++ packages/client_app/firebase/functions/src/index.ts
@@ -32,3 +32,4 @@ export default functions;
 
 export { deleteUser } from "./deleteUser";
 export { onCreateAuthUser } from "./onCreateAuthUser";
+export { beforeUserCreated } from "./beforeUserCreated";
