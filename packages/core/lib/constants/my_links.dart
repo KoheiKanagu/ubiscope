3b077ef471ce1b5758066c75b83a5b798c47e111
KoheiKanagu <kanagu@kingu.dev>
2023-08-23 22:45:52 +0900

    my_links

---
+++ packages/core/lib/constants/my_links.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: avoid_classes_with_only_static_members
+
+class MyLinks {
+  static String issues =
+      'https://github.com/KoheiKanagu/ubiscope/issues/new/choose';
+
+  static String termOfService =
+      'https://github.com/KoheiKanagu/ubiscope/blob/main/assets/terms_of_service.md';
+
+  static String privacyPolicy =
+      'https://github.com/KoheiKanagu/ubiscope/blob/main/assets/privacy_policy.md';
+}
