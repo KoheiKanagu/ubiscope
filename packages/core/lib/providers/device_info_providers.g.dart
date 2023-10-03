98a972022c1dde40ffccb65e1f4e36cfedb97ba6
KoheiKanagu <kanagu@kingu.dev>
2023-10-03 23:03:27 +0900

    generate

--- packages/core/lib/providers/device_info_providers.g.dart
+++ packages/core/lib/providers/device_info_providers.g.dart
@@ -40,4 +40,4 @@ final deviceInfoIsPhysicalDeviceProvider = AutoDisposeProvider<bool>.internal(
 
 typedef DeviceInfoIsPhysicalDeviceRef = AutoDisposeProviderRef<bool>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- packages/core/lib/providers/geohash_providers.g.dart
+++ packages/core/lib/providers/geohash_providers.g.dart
@@ -377,4 +377,4 @@ class _GeohashMeasurementPointSnapshotsProviderElement
       (origin as GeohashMeasurementPointSnapshotsProvider).radiusKm;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
