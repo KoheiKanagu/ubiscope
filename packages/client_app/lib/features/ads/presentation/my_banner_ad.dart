8ba0bada65b23de90f708fbd86fc9dee855185ef
KoheiKanagu <kanagu@kingu.dev>
2023-10-15 16:05:29 +0900

    fix(client_app): failed load bannerAd

--- packages/client_app/lib/features/ads/presentation/my_banner_ad.dart
+++ packages/client_app/lib/features/ads/presentation/my_banner_ad.dart
@@ -1,6 +1,7 @@
 import 'package:client_app/features/ads/application/ad_providers.dart';
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -11,23 +12,48 @@ class MyBannerAd extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final adUnitId = ref.watch(bannerAdUnitIdProvider);
+    final size = ref
+        .watch(
+          bannerAdSizeProvider(
+            MediaQuery.orientationOf(context),
+            MediaQuery.sizeOf(context).width.truncate(),
+          ),
+        )
+        .asData
+        ?.value;
+
+    if (size == null) {
+      return const SizedBox.shrink();
+    }
+
+    final bannerAd = BannerAd(
+      size: size,
+      adUnitId: adUnitId,
+      listener: BannerAdListener(
+        onAdFailedToLoad: (ad, error) {
+          logger.w('onAdFailedToLoad: $ad, $error');
+          ad.dispose();
+        },
+      ),
+      request: const AdRequest(),
+    );
+
+    useEffect(
+      () {
+        bannerAd.load();
+        return bannerAd.dispose;
+      },
+      [bannerAd],
+    );
+
     return SafeArea(
       child: SizedBox(
         height: kToolbarHeight,
         child: MyOutlinedWidget(
-          child: ref
-              .watch(
-                bannerAdControllerProvider(
-                  MediaQuery.orientationOf(context),
-                  MediaQuery.sizeOf(context).width.truncate(),
-                ),
-              )
-              .maybeWhen(
-                orElse: SizedBox.shrink,
-                data: (ad) => AdWidget(
-                  ad: ad,
-                ),
-              ),
+          child: AdWidget(
+            ad: bannerAd,
+          ),
         ),
       ),
     );
